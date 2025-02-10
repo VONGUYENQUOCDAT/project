42b830ac87d840f140f85fef974b378ffad2b522
VONGUYENQUOCDAT <vodatt165@gmail.com>
2025-02-10 21:51:22 +0700

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "library.h"
+
+void displayMenu(){
+	printf("\n===== HE THONG QUAN LY THU VIEN =====\n");
+	printf("1. Hien thi danh sach sach\n");
+	printf("2. Them sach\n");
+	printf("3. Sua thong tin sach\n");
+	printf("4. Thoat\n");
+	printf("Nhap lua chon: ");
+}
